09d4dd6a00743ee1ed4b1b6cdb00efeeb40c958a
Gabriel Cezar <gabriel.cezar.coder@gmail.com>
2024-08-24 21:13:07 -0300

    feat: testing

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Video[] videos = new Video[3];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        videos[0] = new Video("First Video");
+        videos[1] = new Video("Second Video");
+        videos[2] = new Video("Third Video");
+
+        Apprentice[] apprentices = new Apprentice[2];
+
+        apprentices[0] = new Apprentice("John Doe", 30, "M", "johnDoe");
+        apprentices[1] = new Apprentice("Jane Doe", 30, "F", "janeDoe");
+
+        View[] views = new View[2];
+        views[0] = new View(apprentices[0], videos[0]);
+        views[0].review();
+        System.out.println(views[0]);
+
+        views[1] = new View(apprentices[1], videos[0]);
+        views[1].review(87.3f);
+        System.out.println(views[1]);
     }
 }
